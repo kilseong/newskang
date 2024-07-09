0e205650dd517e4a1fa98ca5275e3defbbf7f87e
Kilseong Kang <coladrin2017@gmail.com>
2024-07-10 03:32:10 +0900

    240709 commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const API_KEY = `d0bdfb14f0c84be0b4d39e8615497e5e`;
+let news = [];
+
+const getLatesNews = async()=>{
+  const url = new URL(
+    `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`
+  );
+  
+  const response = await fetch(url);
+  const data = await response.json();
+  news = data.articles;
+  console.log("dddd", news);
+
+};
+
+getLatesNews();
