0a97006c752bb32ffa8b06d1b7037a04cb58c847
Kilseong Kang <coladrin2017@gmail.com>
2024-07-11 10:24:30 +0900

    240711 commit

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const API_KEY = `d0bdfb14f0c84be0b4d39e8615497e5e`;
 const keyword = "";
 const PAGE_SIZE = "10";
-let news = [];
+let newsList = [];
 
 const getLatesNews = async()=>{
   const url = new URL(
@@ -11,9 +11,48 @@ const getLatesNews = async()=>{
   
   const response = await fetch(url);
   const data = await response.json();
-  news = data.articles;
-  console.log("dddd", news);
+  newsList = data.articles;
+  render();
+  console.log("dddd", newsList);
 
 };
 
+const render = () => {
+  const newsHTML =newsList.map((news) => `<div class="row news">
+          <div class="col-lg-4">
+            <img class ="news-img-size" 
+                src =${news.urlToImage} />
+          </div>
+          <div class="col-lg-8">
+            <h2>${news.title}</h2>
+            <p>
+              ${news.description}
+            </p>
+            <div>
+              ${news.source.name} * ${news.publishedAt}
+            </div>
+          </div>
+        </div>`
+        ).join("");
+  
+  document.getElementById("news-board").innerHTML=newsHTML
+}
+
 getLatesNews();
+
+const openNav = () => {
+  document.getElementById("mySidenav").style.width = "250px";
+};
+
+const closeNav = () => {
+  document.getElementById("mySidenav").style.width = "0";
+};
+
+const openSearchBox = () => {
+  let inputArea = document.getElementById("input-area");
+  if (inputArea.style.display === "inline") {
+    inputArea.style.display = "none";
+  } else {
+    inputArea.style.display = "inline";
+  }
+};
